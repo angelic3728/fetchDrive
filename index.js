0b257f71f07f33ea40e17979a877a29e5634d7d3
dream-builder48 <74439927+dream-builder48@users.noreply.github.com>
2021-12-10 23:46:08 +0300

    add new repo

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+const http = require('http');
+const getfilelist = require("google-drive-getfilelist");
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  var folder_id = req.query['folder_id'];
+  getfilelist.GetFileList(
+    {
+      auth: "AIzaSyClNXOohHrbsQFYrz_jx_c_sVH9ry8Ec1M",
+      fields: "files(id)",
+      id: folder_id,
+    },
+    (err, resp) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const fileList = resp.fileList.flatMap(({ files }) => files);
+      res.render('home', {
+         file_list:fileList 
+      });
+    }
+  );
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
